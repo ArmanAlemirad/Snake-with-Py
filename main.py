86e5b9a446e4cb1f372c5a09514ab89d55de9207
Arman Alemirad <arman.alemirad@gmail.com>
2024-02-13 12:08:32 +0100

    Add variables

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello World")
-print("Hej")
+
+# Variabler, string
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+print("Hej " + full_name)
+# print(type(full_name))
+
+# integer
+age = 20
+age += 1
+print("Your Age is: " + str(age))
+# print(type(age))
+
+# float
+height = 100.5
+print("Your height is: " + str(height))
+#print(type(height))
+
+# boolean
+human = True
+print("Are you a human: " + str(human))
