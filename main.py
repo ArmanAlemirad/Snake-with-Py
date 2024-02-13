8e854c89d0ae404dbc6f5758bcfd2e3db749568d
Arman Alemirad <arman.alemirad@gmail.com>
2024-02-13 15:13:36 +0100

    Learn random

--- main.py
+++ main.py
@@ -88,3 +88,17 @@
 #     print("Have a nice day")
 #
 # hello("Bro","Code", 21)
+#
+# #Random
+# import random
+# x = random.randint(1,6)
+# y = random.random()
+#
+# myList = ['rock','paper','scissors']
+# z = random.choice(myList)
+#
+# cards = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+#
+# random.shuffle(cards)
+#
+# print(cards)
