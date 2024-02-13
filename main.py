df990484515a7b59443246432a04531727c9aeb3
Arman Alemirad <arman.alemirad@gmail.com>
2024-02-13 14:16:42 +0100

    Learn Input from user

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
+#
+# # Variabler, string
+# first_name = "Bro"
+# last_name = "Code"
+# full_name = first_name + " " + last_name
+# print("Hej " + full_name)
+# # print(type(full_name))
+#
+# # integer
+# age = 20
+# age += 1
+# print("Your Age is: " + str(age))
+# # print(type(age))
+#
+# # float
+# height = 100.5
+# print("Your height is: " + str(height))
+# #print(type(height))
+#
+# # boolean
+# human = True
+# print("Are you a human: " + str(human))
+#
+#
+# # name, age, attractive = "Bro", 21, True
+# # print(name, age, attractive)
+#
+# #String methods
+# name = "Bro Code"
+# print(len(name))
+# print(name.find("B"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("o"))
+# print(name.replace("Bro Code", "Kalle Anka"))
 
-# Variabler, string
-first_name = "Bro"
-last_name = "Code"
-full_name = first_name + " " + last_name
-print("Hej " + full_name)
-# print(type(full_name))
-
-# integer
-age = 20
-age += 1
-print("Your Age is: " + str(age))
-# print(type(age))
-
-# float
-height = 100.5
-print("Your height is: " + str(height))
-#print(type(height))
-
-# boolean
-human = True
-print("Are you a human: " + str(human))
+#input
+# name = input("What is your name? ")
+# print("Hello " + name + "!")
+#
+# age = int(input("How old are you? "))
+# print("You are " + str(age) + " years")
