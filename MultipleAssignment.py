749f685b20a0ee91beea92d08ec35150817ed97f
Arman Alemirad <arman.alemirad@gmail.com>
2024-02-13 12:19:40 +0100

    Add String methods

---
+++ MultipleAssignment.py
@@ -0,0 +1,16 @@
+
+
+# name, age, attractive = "Bro", 21, True
+# print(name, age, attractive)
+
+#String methods
+name = "Bro Code"
+print(len(name))
+print(name.find("B"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("Bro Code", "Kalle Anka"))
