3ae468e862774019a4ef6569a3b109f7a124bc1d
Arman Alemirad <arman.alemirad@gmail.com>
2024-02-15 10:25:56 +0100

    Add arrow key function to move the snake

--- main.py
+++ main.py
@@ -66,7 +66,20 @@ def next_turn(snake, food):
 
 
 def change_direction(new_direction):
-    pass
+    global direction
+
+    if new_direction == "left":
+        if direction != "righ":
+            direction = new_direction
+    elif new_direction == "right":
+        if direction != "left":
+            direction = new_direction
+    elif new_direction == "up":
+        if direction != "down":
+            direction = new_direction
+    elif new_direction == "down":
+        if direction != "up":
+            direction = new_direction
 
 
 def check_collision():
@@ -100,6 +113,10 @@ x = int((screen_width - window_width) / 2)
 y = int((screen_height - window_height) / 2)
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+window.bind("<Left>", lambda event: change_direction('left'))
+window.bind("<Right>", lambda event: change_direction('right'))
+window.bind("<Up>", lambda event: change_direction('up'))
+window.bind("<Down>", lambda event: change_direction('down'))
 
 
 snake = Snake()
